3f17030543b99d55d1964dd0d2bddcc4a26d6358
MayankGit12 <167399774+MayankGit12@users.noreply.github.com>
2024-04-18 11:08:57 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="testr.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="width: 778px; margin-left: 161px;">
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Enter Name:"></asp:Label>
+       
+        <asp:TextBox ID="TextBox5" runat="server" style="margin-left: 35px" Width="318px"></asp:TextBox>
+       
+        <p>
+            Enter phone<asp:TextBox ID="TextBox2" runat="server" style="margin-left: 45px" Width="317px"></asp:TextBox>
+        </p>
+        <p>
+&nbsp;Enter Address:&nbsp;<asp:TextBox ID="TextBox3" runat="server" style="margin-left: 26px" Width="314px"></asp:TextBox>
+        </p>
+        <p>
+            Current Location:
+            <asp:TextBox ID="TextBox4" runat="server" style="margin-left: 11px" Width="310px"></asp:TextBox>
+        </p>
+        <p>
+            Attach Resume:<asp:FileUpload ID="FileUpload1" runat="server" style="margin-left: 28px" Width="339px" />
+        </p>
+&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-left: 141px" Text="Submit" Width="266px" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Web.UI.WebControls;
+
+namespace testr
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string smtpServer = "smtp.gmail.com";
+        int smtpPort = 587;
+        string smtpUsername = "shimanshu0507@gmail.com";
+        string smtpPassword = "qhyf xduc mqon vqoc";
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            string tb1 = TextBox5.Text;
+            string tb2 = TextBox2.Text;
+            string tb3 = TextBox3.Text;
+            string tb4 = TextBox4.Text;
+
+           
+            if (string.IsNullOrWhiteSpace(tb1) || string.IsNullOrWhiteSpace(tb2) || string.IsNullOrWhiteSpace(tb3) || string.IsNullOrWhiteSpace(tb4))
+            {
+                Response.Write("Please fill out all required fields.");
+                return;
+            }
+
+        
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(smtpUsername);
+            message.To.Add(new MailAddress("lalbabu.shukla.145@gmail.com"));
+            message.Subject = "System generated mail and need to repl y";
+            message.Body = $"Name: {tb1}\nPhone: {tb2}\nCurrent Location: {tb3}\nAddress: {tb4}";
+
+            
+            if (FileUpload1.HasFile)
+            {
+                try
+                {
+                   
+                    string fileName = Path.GetFileName(FileUpload1.FileName);
+                    string folderPath = Server.MapPath("~/uploads/");
+                    string filePath = Path.Combine(folderPath, fileName); 
+
+                   
+                    FileUpload1.SaveAs(filePath);
+
+                   
+                    Attachment attachment = new Attachment(filePath);
+                    message.Attachments.Add(attachment);
+                }
+                catch (Exception ex)
+                {
+                   
+                    Response.Write("Error uploading file: " + ex.Message);
+                    return; 
+                }
+            }
+
+            // Send email
+            SmtpClient smtpClient = new SmtpClient(smtpServer, smtpPort);
+            smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+            smtpClient.EnableSsl = true;
+
+            try
+            {
+                smtpClient.Send(message);
+                Response.Write("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error sending email: " + ex.Message);
+            }
+        }
+    }
+}
